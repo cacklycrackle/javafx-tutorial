bb78d397d1a332d97e11a1a2edca500d254edc3d
nightlycacklewitch <151456716+cacklycrackle@users.noreply.github.com>
2026-09-02 10:45:48 +0800

    Add limits to window size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,9 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            // stage.setMaxWidth(417); // Add if elements not automatically resizable horizontally
             fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
